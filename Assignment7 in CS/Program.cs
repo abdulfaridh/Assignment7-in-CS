5a56a1d8a3a2fb55b214c71904a59513602715a6
farid <farid@LAPTOP-SPTVSQVM>
2024-08-15 09:23:09 +0530

    Add project files.

---
+++ Assignment7 in CS/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment7
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter b:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter c:");
+        int c = int.Parse(Console.ReadLine());
+        if(a>b && a>c)
+        {
+            Console.WriteLine($"{a} is the greatest among three");
+        }
+        else if(b>c)
+        {
+            Console.WriteLine($"{b} is the greatest among three");
+        }
+        else
+        {
+            Console.WriteLine($"{c} is the greatest among three");
+        }
+    }
+}
